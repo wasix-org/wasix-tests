582c4d011aaaba02046c018cac0bedbc72a6b1d7
Zebreus <zebreus@zebre.us>
2025-08-28 13:00:59 +0200

    Add tests for exiting in dyncalls in threads

---
+++ exit-nonzero-in-dyncall-thread/test.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+set -euo pipefail
+cd "$(dirname "$0")"
+source ../lib/assert.sh
+source ../lib/test-utils.sh
+
+make main
+set +e
+run main
+exitcode=$?
+
+assert_eq "Dyncall in thread" "$(cat stdout.log)" "stdout did not match expected value"
+assert_eq "" "$(cat stderr.log)" "stderr did not match expected value"
+set -e
+
+assert_eq 99 "$exitcode" "Exit code was not 99, got $exitcode"
