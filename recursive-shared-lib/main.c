c78471a45bfaaffac33a56db608def5a35797df5
Zebreus <zebreus@zebre.us>
2025-09-02 13:48:26 +0200

    Add comment to recursive-shared-lib

--- recursive-shared-lib/main.c
+++ recursive-shared-lib/main.c
@@ -3,6 +3,7 @@
 
 extern int get_value();
 
+// Currently broken on wasix, because wasm-ld does not support linking against the library that is being built yet.
 int main() {
     int side_value = get_value();
     printf("The shared library returned: %i\n", side_value);
