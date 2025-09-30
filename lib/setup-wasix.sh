eda10a818fbc9660994c76caa7cddc2bb71ee8fd
Zebreus <zebreus@zebre.us>
2025-09-30 19:38:23 +0200

    Move wasix setup to the CI job

--- lib/setup-wasix.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-
-sudo apt update > /dev/null
-sudo apt install -y clang llvm git git-lfs build-essential make cmake libtool pkg-config yq  > /dev/null
-
-curl -sSf https://raw.githubusercontent.com/wasix-org/wasix-clang/refs/heads/main/setup.sh | bash
